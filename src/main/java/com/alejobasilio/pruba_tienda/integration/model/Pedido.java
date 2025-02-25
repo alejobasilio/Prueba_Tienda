364d470ef4b49f56569bfd571507c6986657c364
alejo.basilio <alejo.basilio@viewnext.com>
2025-02-25 13:44:29 +0100

    Integration Model Pedido terminado

--- src/main/java/com/alejobasilio/pruba_tienda/integration/model/Pedido.java
+++ src/main/java/com/alejobasilio/pruba_tienda/integration/model/Pedido.java
@@ -2,6 +2,14 @@ package com.alejobasilio.pruba_tienda.integration.model;
 
 import java.util.List;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +19,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "PEDIDO")
 class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@ElementCollection
+	@JoinTable(name = "LISTA_PRODUCTO", joinColumns = @JoinColumn(name="ID_PRODUCTOS"))
 	private List<Producto> listaProductos;
 }
