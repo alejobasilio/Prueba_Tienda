06134aff0b751212158105d23b976a81b0d8f9ab
alejo.basilio <alejo.basilio@viewnext.com>
2025-02-27 13:25:03 +0100

    Javadoc business-dtos Entrada

--- src/main/java/com/alejobasilio/pruba_tienda/business/dtos/CarritoDTOEntrada.java
+++ src/main/java/com/alejobasilio/pruba_tienda/business/dtos/CarritoDTOEntrada.java
@@ -5,6 +5,14 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Clase que representa la lista de productos de un pedido.
+ * Los Dtos que contienen la palabra entrada son Clases que envia el usuario 
+ * 
+ * @author Alejo Basilio Alfonso
+ * @version 1.0
+ * @since 1.0
+ */
 @Getter
 @Setter
 public class CarritoDTOEntrada {
--- src/main/java/com/alejobasilio/pruba_tienda/business/dtos/ProductoDTOEntrada.java
+++ src/main/java/com/alejobasilio/pruba_tienda/business/dtos/ProductoDTOEntrada.java
@@ -3,6 +3,14 @@ package com.alejobasilio.pruba_tienda.business.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Clase que representa los productos de un pedido.
+ * Los Dtos que contienen la palabra entrada son Clases que envia el usuario 
+ * 
+ * @author Alejo Basilio Alfonso
+ * @version 1.0
+ * @since 1.0
+ */
 @Getter
 @Setter
 public class ProductoDTOEntrada {
