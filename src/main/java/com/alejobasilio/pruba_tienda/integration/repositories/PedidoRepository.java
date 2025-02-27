63a7b5a0f6b9a806a0e03224a7c74ab68a1f651c
alejo.basilio <alejo.basilio@viewnext.com>
2025-02-27 13:10:07 +0100

    Javadoc integration-repositories

--- src/main/java/com/alejobasilio/pruba_tienda/integration/repositories/PedidoRepository.java
+++ src/main/java/com/alejobasilio/pruba_tienda/integration/repositories/PedidoRepository.java
@@ -8,8 +8,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alejobasilio.pruba_tienda.integration.model.Pedido;
 
+/**
+ * Repositorio para interactuar con la tabla de pedidos.
+ * 
+ * Este repositorio proporciona métodos para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en la tabla de pedidos.
+ * 
+ * @author Alejo Basilio Alfonso
+ * @version 1.0
+ */
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+	/**
+	 * Método que devuelve una lista pedidos por ids
+	 * @param ids List<Long> 
+	 * @return
+	 */
 	@Query("SELECT DISTINCT p FROM Pedido p JOIN p.listaProductos lp WHERE lp.id IN :ids")
 	List<Pedido> pedidosPorIds(@RequestParam("ids") List<Long> ids);
 
--- src/main/java/com/alejobasilio/pruba_tienda/integration/repositories/ProductoRepository.java
+++ src/main/java/com/alejobasilio/pruba_tienda/integration/repositories/ProductoRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alejobasilio.pruba_tienda.integration.model.Producto;
 
+/**
+ * Repositorio para interactuar con la tabla de productos.
+ * 
+ * Este repositorio proporciona métodos para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en la tabla de productos.
+ * 
+ * @author Alejo Basilio Alfonso
+ * @version 1.0
+ */
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
 }
