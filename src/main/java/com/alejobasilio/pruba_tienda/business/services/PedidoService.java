29eca2c3a8d90c05d36a09dae911754abce5ee93
alejo.basilio <alejo.basilio@viewnext.com>
2025-02-27 08:52:29 +0100

    Creación de servicios con sus implementaciones

--- src/main/java/com/alejobasilio/pruba_tienda/business/services/PedidoService.java
+++ src/main/java/com/alejobasilio/pruba_tienda/business/services/PedidoService.java
@@ -1,5 +1,16 @@
 package com.alejobasilio.pruba_tienda.business.services;
 
-public interface PedidoServices {
+import java.util.List;
+import java.util.Optional;
 
+import com.alejobasilio.pruba_tienda.business.dtos.PedidoDTOEntrada;
+import com.alejobasilio.pruba_tienda.business.dtos.PedidoDTOSalida;
+
+public interface PedidoService {
+	
+    PedidoDTOSalida transformarPedido(PedidoDTOEntrada pedidoDTO);
+    
+    Optional<PedidoDTOSalida> obtenerPedidoPorId(Long id);
+    
+    List<PedidoDTOSalida> obtenerTodosLosPedidos();
 }
