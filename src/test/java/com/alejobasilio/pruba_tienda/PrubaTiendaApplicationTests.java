e980fde5219f70c47cf3ba2550491ac6d9119a26
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-05 09:26:23 +0100

    Test Main

--- src/test/java/com/alejobasilio/pruba_tienda/PrubaTiendaApplicationTests.java
+++ src/test/java/com/alejobasilio/pruba_tienda/PrubaTiendaApplicationTests.java
@@ -3,7 +3,7 @@ package com.alejobasilio.pruba_tienda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class PrubaTiendaApplicationTests {
 
 	@Test
