f783511044408a22cfa1ee2bee40364af572711a
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-05 11:23:10 +0100

    Documentación Swagger de Controller

--- src/main/java/com/alejobasilio/pruba_tienda/presentation/restcontroller/PedidoController.java
+++ src/main/java/com/alejobasilio/pruba_tienda/presentation/restcontroller/PedidoController.java
@@ -13,6 +13,12 @@ import com.alejobasilio.pruba_tienda.business.dtos.PedidoDTOEntrada;
 import com.alejobasilio.pruba_tienda.business.dtos.PedidoDTOSalida;
 import com.alejobasilio.pruba_tienda.business.services.PedidoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * Controlador REST que maneja las solicitudes relacionadas con pedidos.
  * 
@@ -25,32 +31,37 @@ import com.alejobasilio.pruba_tienda.business.services.PedidoService;
 public class PedidoController {
 
 	private PedidoService pedidoService;
-	
-	
-	
+
 	public PedidoController(PedidoService pedidoService) {
 		super();
 		this.pedidoService = pedidoService;
 	}
 
-
-
 	/**
-     * Método que crea un nuevo pedido.
-     * 
-     */
+	 * Método que crea un nuevo pedido.
+	 * 
+	 */
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-    public PedidoDTOSalida crearPedido(@RequestBody PedidoDTOEntrada pedidoDTO) {
-        return pedidoService.transformarPedido(pedidoDTO);
-    }
-	
-    /**
-     * Método que borra un pedido por su ID.
-     * 
-     */
+	@Operation(summary = "Crear un nuevo pedido", description = "Crea un nuevo pedido con los datos proporcionados")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Pedido creado correctamente", content = @Content(schema = @Schema(implementation = PedidoDTOSalida.class))),
+			@ApiResponse(responseCode = "400", description = "Datos de pedido inválidos") 
+		})
+	public PedidoDTOSalida crearPedido(@RequestBody PedidoDTOEntrada pedidoDTO) {
+		return pedidoService.transformarPedido(pedidoDTO);
+	}
+
+	/**
+	 * Método que borra un pedido por su ID.
+	 * 
+	 */
+	@Operation(summary = "Borrar un pedido", description = "Borra un pedido por su ID")
+	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Pedido borrado correctamente"),
+			@ApiResponse(responseCode = "404", description = "Pedido no encontrado") 
+	})
 	@DeleteMapping("/{id}")
 	public void borrarPedido(@PathVariable Long id) {
-	    pedidoService.borrarPedidoById(id);
+		pedidoService.borrarPedidoById(id);
 	}
 }
