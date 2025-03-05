b7bf5eaf39600d75c5e84f5ada20abe962a571f9
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-05 10:22:04 +0100

    Cambio de lugar de swagger

--- src/main/java/com/alejobasilio/pruba_tienda/business/dtos/CaracteristicasDTOSalida.java
+++ src/main/java/com/alejobasilio/pruba_tienda/business/dtos/CaracteristicasDTOSalida.java
@@ -3,8 +3,7 @@ package com.alejobasilio.pruba_tienda.business.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 /**
  * Clase que representa las características de un producto.
  * Los Dtos que contienen la palabra salida son Clases que se devolveran al usuario
@@ -13,6 +12,8 @@ import lombok.Setter;
  * @version 1.0
  * @since 1.0
  */
+@Getter
+@Setter
 public class CaracteristicasDTOSalida {
 
 	private String nombre;
